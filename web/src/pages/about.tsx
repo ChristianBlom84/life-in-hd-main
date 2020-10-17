5c10408225745218a207bf44c8ca18f04315537d
Christian Blom <christian@christianblom.se>
2020-10-17 18:13:42 +0200

    Adding contact form.

--- web/src/pages/about.tsx
+++ web/src/pages/about.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SEO from '../components/Seo';
 import styles from './about.module.scss';
 
-const AboutPage: React.FC = () => (
-  <>
-    <SEO title="About us | Life in HD" />
-    <section className={styles.section}>
-      <div className={styles.hero}>
-        <h1 className={styles.h1}>About us</h1>
-        <p>Here we will have information about us.</p>
-      </div>
-    </section>
-  </>
-);
+const AboutPage: React.FC = () => {
+  return (
+    <>
+      <SEO title="About us | Life in HD" />
+      <section className={styles.section}>
+        <div className={styles.hero}>
+          <h1 className={styles.h1}>About us</h1>
+          <p>Here we will have information about us.</p>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default AboutPage;
--- web/src/pages/contact.tsx
+++ web/src/pages/contact.tsx
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SEO from '../components/Seo';
+import styles from './contact.module.scss';
 
-const ContactPage: React.FC = () => (
-  <>
-    <SEO title="Contact us | Life in HD" />
-    <section>
-      <h1>Contact us</h1>
-      <p>
-        Use the form below to contact us and we&apos;ll get back to you as soon
-        as we can.
-      </p>
-    </section>
-  </>
-);
+const ContactPage: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  return (
+    <>
+      <SEO title="Contact us | Life in HD" />
+      <section>
+        <h1>Contact us</h1>
+        <p>
+          Use the form below to contact us and we&apos;ll get back to you as
+          soon as we can.
+        </p>
+        <div>
+          <form
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <div className={styles.inputGroup}>
+              <label htmlFor="name">Your Name:</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={name}
+                onChange={(e: React.FormEvent<HTMLInputElement>): void =>
+                  setName(e.currentTarget.value)
+                }
+              />
+            </div>
+            <div className={styles.inputGroup}>
+              <label htmlFor="email">Your Email:</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={email}
+                onChange={(e: React.FormEvent<HTMLInputElement>): void =>
+                  setEmail(e.currentTarget.value)
+                }
+              />
+            </div>
+            <div className={styles.inputGroup}>
+              <label htmlFor="email">Message:</label>
+              <input
+                type="text"
+                name="message"
+                id="message"
+                value={message}
+                onChange={(e: React.FormEvent<HTMLInputElement>): void =>
+                  setMessage(e.currentTarget.value)
+                }
+              />
+            </div>
+            <input type="hidden" name="form-name" value="contact" />
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default ContactPage;
