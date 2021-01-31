c41fae12333657bc22bb4d37e7cdc86894566c57
Christian Blom <christian@christianblom.se>
2021-01-31 16:01:08 +0100

    Front page done, will tweak sub pages on a different branch.

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -30,14 +30,42 @@ const IndexPage: React.FC = () => {
           }
         }
       }
-      heroBackground: file(relativePath: { eq: "backgrounds/start_hero.jpg" }) {
+      backgroundTwo: file(relativePath: { eq: "backgrounds/start_bg2.jpg" }) {
         childImageSharp {
           fluid(quality: 100) {
             ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
-      backgroundTwo: file(relativePath: { eq: "backgrounds/start_bg2.jpg" }) {
+      backgroundThree: file(relativePath: { eq: "backgrounds/start_bg3.jpg" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      backgroundFour: file(relativePath: { eq: "backgrounds/start_bg4.jpg" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      backgroundFive: file(relativePath: { eq: "backgrounds/start_bg5.jpg" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      backgroundSix: file(relativePath: { eq: "backgrounds/start_bg6.jpg" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      heroBackground: file(relativePath: { eq: "backgrounds/start_hero.jpg" }) {
         childImageSharp {
           fluid(quality: 100) {
             ...GatsbyImageSharpFluid_withWebp
@@ -51,7 +79,12 @@ const IndexPage: React.FC = () => {
   const hexagramOne = data.hexagramOne.childImageSharp.fluid;
   const hexagramTwo = data.hexagramTwo.childImageSharp.fluid;
   const heroBackground = data.heroBackground.childImageSharp.fluid;
-  // const backgroundTwo = data.backgroundTwo.childImageSharp.fluid;
+  console.log(data.backgroundTwo);
+  const backgroundTwo = data.backgroundTwo.childImageSharp.fluid;
+  const backgroundThree = data.backgroundThree.childImageSharp.fluid;
+  const backgroundFour = data.backgroundFour.childImageSharp.fluid;
+  const backgroundFive = data.backgroundFive.childImageSharp.fluid;
+  const backgroundSix = data.backgroundSix.childImageSharp.fluid;
 
   return (
     <>
@@ -76,48 +109,114 @@ const IndexPage: React.FC = () => {
       <section className={styles.contentSection}>
         <BackgroundImage
           Tag={`section`}
-          id={`hero`}
-          className={styles.background}
-          fluid={data.backgroundTwo.childImageSharp.fluid}
+          className={`${styles.background} ${styles.justifyCenter}`}
+          fluid={backgroundTwo}
         >
-          <div className={styles.contentOverlay}>
-            <h3>What is Human Design?</h3>
-            <p>
-              Human Design is an intricate, amazing system for self-discovery
-              and integrity. The core principle of Human Design is that your
-              mind is an amazing tool - but it is not meant to make decisions
-              for you in your life. Human Design shows you how you as a unique
-              individual are made to make correct decisions and gives you the
-              tools to find out the truth for yourself.
-            </p>
-            <p>
-              Human Design is not a religion and there is no dogma. There is
-              only a mapping of the mechanical aspects that lie beneath and
-              drive life as we know it, and the individual exploration possible
-              through experimentation with your own correct decision making.
-            </p>
-          </div>
+          <Parallax y={['-75px', '75px']}>
+            <div className={`${styles.contentOverlay} ${styles.mAuto}`}>
+              <h3>What is Human Design?</h3>
+              <p>
+                Human Design is an intricate, amazing system for self-discovery
+                and integrity. The core principle of Human Design is that your
+                mind is an amazing tool - but it is not meant to make decisions
+                for you in your life. Human Design shows you how you as a unique
+                individual are made to make correct decisions and gives you the
+                tools to find out the truth for yourself.
+              </p>
+              <p className={styles.mb0}>
+                Human Design is not a religion and there is no dogma. There is
+                only a mapping of the mechanical aspects that lie beneath and
+                drive life as we know it, and the individual exploration
+                possible through experimentation with your own correct decision
+                making.
+              </p>
+            </div>
+          </Parallax>
         </BackgroundImage>
-        <div className={styles.content}>
-          <p>
-            Have you struggled with a feeling of frustration? Maybe you’re
-            health has suffered? Or you’ve felt caught in situations or
-            relationships you just couldn’t get out of? Let the anger get the
-            best of you?
-          </p>
-          <p>
-            Do you want to get information on how to make decisions in a way
-            that is correct for you? A good place to start understanding how to
-            navigate is having a <Link to="services">reading</Link> of your
-            Human Design chart. If you’ve already had a reading and want to dive
-            deeper, then a <Link to="services">Living Your Design course</Link>{' '}
-            could be an option.
-          </p>
-          <p>
-            Are you ready to learn how to navigate life in a way that is correct
-            for you?
-          </p>
-        </div>
+        <BackgroundImage
+          Tag={`section`}
+          className={`${styles.background} ${styles.justifyStart}`}
+          fluid={backgroundThree}
+        >
+          <Parallax y={['-100px', '100px']}>
+            <div className={`${styles.contentOverlay} ${styles.ml4}`}>
+              <p className={styles.mb0}>
+                Here at Life in HD we offer{' '}
+                <Link to="/services">readings, sessions and classes</Link> to
+                help you get started on your own, personal journey of living
+                correctly as yourself. Often we need information, support or
+                sharing coming from outside ourselves in order to trigger a
+                change in our direction. Is this what you need? If this
+                information resonates with you, we invite you to{' '}
+                <Link to="/contact">contact us</Link>.
+              </p>
+            </div>
+          </Parallax>
+        </BackgroundImage>
+        <BackgroundImage
+          Tag={`section`}
+          className={`${styles.background} ${styles.justifyEnd}`}
+          fluid={backgroundFour}
+        >
+          <Parallax y={['50px', '-50px']}>
+            <div
+              className={`${styles.contentOverlay} ${styles.mr4} ${styles.mlAuto}`}
+            >
+              <h3>Foundation reading with Christian</h3>
+              <p className={styles.mb0}>
+                A foundation reading is the natural place to start your Human
+                Design experiment. You will learn the elemental aspects and
+                challenges of your particular design and become familiar with
+                the core concepts of Type, Strategy and Inner Authority.
+              </p>
+            </div>
+          </Parallax>
+        </BackgroundImage>
+        <BackgroundImage
+          Tag={`section`}
+          className={`${styles.background} ${styles.justifyCenter}`}
+          fluid={backgroundFive}
+        >
+          <Parallax y={['100px', '-100px']}>
+            <div className={`${styles.contentOverlay} ${styles.mAuto}`}>
+              <h3>Manifestor sessions with Milla</h3>
+              <p className={styles.mb0}>
+                Do you have a Manifestor in your life? Are you struggling with
+                communication and wondering how to best deal with this person?
+                I’m inviting you to reach out for a Manifestor session with me,
+                where you can ask all your questions and I’ll give you an inside
+                aura perspective. To be clear: I will not be speaking from
+                anything other than my direct experience and is therefore
+                unable, and unwilling, to answer on someone else’s behalf.
+              </p>
+            </div>
+          </Parallax>
+        </BackgroundImage>
+        <BackgroundImage
+          Tag={`section`}
+          className={`${styles.background} ${styles.justifyStart}`}
+          fluid={backgroundSix}
+        >
+          <Parallax y={['-100px', '100px']}>
+            <div
+              className={`${styles.contentOverlay} ${styles.ml4} ${styles.mrAuto}`}
+            >
+              <h3>Who are we?</h3>
+              <p>
+                Milla and Christian, (6/2 Manifestor and 4/6 Projector) have
+                been married since 2013. Christian had his first Human Design
+                reading February 2007 and Milla started her experiment June
+                2012. They are raising their two kids, Leia (5/1 Manifesting
+                Generator) and Elektra (6/2 Generator), according to their
+                design.
+              </p>
+              <p className={styles.mb0}>
+                Christian is a certified Human Design Analyst and Living Your
+                Design Guide. Milla is a certified Living Your Design Guide.
+              </p>
+            </div>
+          </Parallax>
+        </BackgroundImage>
       </section>
     </>
   );
