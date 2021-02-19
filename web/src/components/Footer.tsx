a3393734170099dba09252de3d6664138bb56510
Christian Blom <christian@christianblom.se>
2021-02-19 18:30:07 +0100

    Commit for switching computers.

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Image from 'gatsby-image';
-import { SiInstagram } from 'react-icons/si';
+import { SiInstagram, SiFacebook } from 'react-icons/si';
 import styles from './Footer.module.scss';
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <p>© {new Date().getFullYear()} Life in HD</p>
-      <SiInstagram size="1.5rem" />
+      <a
+        className={styles.mr4}
+        href="https://www.facebook.com/Life-in-HD-102470187893894"
+      >
+        <SiFacebook size="1.5rem" />
+      </a>
+      <a href="https://www.instagram.com/lifeinhd.se/">
+        <SiInstagram size="1.5rem" />
+      </a>
     </footer>
   );
 };
