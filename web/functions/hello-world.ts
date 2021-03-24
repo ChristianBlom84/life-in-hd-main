62a7716bfdaea9b8cab54ecdd375e5f23dfb7722
Christian Blom <christian@christianblom.se>
2021-03-24 14:10:26 +0100

    Trying to fix Netlify functions.

--- web/functions/hello-world.ts
+++ web/functions/hello-world.ts
@@ -1,7 +1,11 @@
-// /functions/hello-world.ts
-exports.handler = async function (): Promise<any> {
-  return {
-    statusCode: 200,
-    body: 'Hello world!',
-  };
-};
+// For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
+export function handler(event, context, callback) {
+  console.log('queryStringParameters', event.queryStringParameters);
+  callback(null, {
+    // return null to show no errors
+    statusCode: 200, // http status code
+    body: JSON.stringify({
+      msg: 'Hello, World! ' + Math.round(Math.random() * 10),
+    }),
+  });
+}
--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,5 +1,11 @@
 /* eslint-disable @typescript-eslint/camelcase */
 const siteUrl = 'http://localhost:8000';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createProxyMiddleware } = require('http-proxy-middleware'); //v1.x.x
+// Use implicit require for v0.x.x of 'http-proxy-middleware'
+// const proxy = require('http-proxy-middleware')
+// be sure to replace 'createProxyMiddleware' with 'proxy' where applicable
+
 module.exports = {
   siteMetadata: {
     title: `Life in HD`,
@@ -9,6 +15,17 @@ module.exports = {
     keywords: `Human Design, Human Design Reading, Splenic Center, Ajna Center, Head Center, G Center, Sacral Center, Root Center, Solar Plexus Center, Throat Center,
     Generator, Manifesting Generator, Projector, Reflector, Manifestor`,
     image: `${siteUrl}/images/icons/life-in-hd-icon.jpg`,
+  },
+  developMiddleware: app => {
+    app.use(
+      '/.netlify/functions/',
+      createProxyMiddleware({
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          '/.netlify/functions/': '',
+        },
+      })
+    );
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
