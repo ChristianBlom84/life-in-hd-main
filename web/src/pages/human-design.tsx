fdc06d76433d751eba29a09a3ecd031dc4f974e9
Christian Blom <christian@christianblom.se>
2021-05-24 12:52:50 +0200

    Redesigning and adding info to Human Design page.

--- web/src/pages/human-design.tsx
+++ web/src/pages/human-design.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
+import { Parallax } from 'react-scroll-parallax';
 import Img from 'gatsby-image';
 import SEO from '../components/Seo';
 import styles from './human-design.module.scss';
@@ -14,42 +16,77 @@ const HumanDesignPage: React.FC = () => {
           }
         }
       }
+      bodygraph: file(relativePath: { eq: "bodygraph.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid_noBase64
+          }
+        }
+      }
+      backgroundHumanDesign: file(
+        relativePath: { eq: "backgrounds/humandesign_bg.jpg" }
+      ) {
+        childImageSharp {
+          fluid(quality: 80, srcSetBreakpoints: [800, 1200, 1600, 2500, 4032]) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
     }
   `);
 
   const { fluid } = data.placeholderImage.childImageSharp;
+  const backgroundHumanDesign =
+    data.backgroundHumanDesign.childImageSharp.fluid;
+  const bodygraph = data.bodygraph.childImageSharp.fluid;
 
   return (
     <>
       <SEO title="Human Design Information | Life in HD" />
-      <section className={styles.hero}>
-        <h1>
-          <span>The</span>
-          <span>Human</span>
-          <span>Design</span>
-          <span>System</span>
-        </h1>
-      </section>
-      <section className={styles.content}>
-        <div className={styles.imageParagraph}>
-          <h2>What is Human Design?</h2>
-          <p>
-            Human Design is an intricate, amazing system for self-discovery and
-            integrity. The core principle of Human Design is that your mind is
-            an amazing tool - but it is not meant to make decisions for you in
-            your life. Human Design shows you how you as a unique individual are
-            made to make correct decisions and gives you the tools to find out
-            the truth for yourself.
-          </p>
-          <p>
-            Human Design is not a religion and there is no dogma. There is only
-            a mapping of the mechanical aspects that lie beneath and drive life
-            as we know it, and the individual exploration possible through
-            experimentation with your own correct decision making.
-          </p>
-          <Img className={styles.planetImage} fluid={fluid} />
+      <BackgroundImage
+        Tag={`section`}
+        id={`hero`}
+        className={styles.background}
+        fluid={backgroundHumanDesign}
+      >
+        <div className={styles.heroOverlay}>
+          <Parallax y={['0px', '50px']}>
+            <div className={styles.heroText}>
+              <h1 className={styles.heroHeading}>The Human Design System</h1>
+            </div>
+          </Parallax>
+          <Parallax y={['100px', '50px']}>
+            <div className={`${styles.contentOverlay} ${styles.mAuto}`}>
+              <h2>Human Design Basics</h2>
+              <div className={styles.imageRight}>
+                <p>
+                  <figure className={styles.bodygraphFigure}>
+                    <Img fluid={bodygraph} className={styles.bodygraph} />
+                    <figcaption>The Bodygraph</figcaption>
+                  </figure>
+                  Human Design is a synthesis of several ancient and modern
+                  systems of knowing. Aspects of the I&apos;Ching, Astrology,
+                  Kabbalah&apos;s Tree of Life and the Hindu Brahman Chakra
+                  System come together with genetics and physics through the
+                  influence of neutrinos and are synthesized in what&apos;s
+                  called the Bodygraph.
+                </p>
+              </div>
+              <p>
+                At the center of Human Design is the revelation that every human
+                is a duality - there&apos;s the Design, what&apos;s colored in
+                red in the chart, which is the body and the unconscious. The
+                other aspect is what we call Personality, the conscious part of
+                us, who we think we are and this is what&apos;s colored in
+                black. These aspects are held together by the Magnetic Monopole,
+                a magnetic force which only attracts, and gives us form as
+                unique individuals.
+              </p>
+              <Img className={styles.planetImage} fluid={fluid} />
+            </div>
+          </Parallax>
         </div>
-      </section>
+      </BackgroundImage>
     </>
   );
 };
