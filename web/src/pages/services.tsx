feba1956e9594e1d096854b3f313d94a9a5fba1d
Christian Blom <christian@christianblom.se>
2022-09-24 14:18:50 +0200

    Updated node_sass Gatsby integrated and added blurb with payment button for Manifestor Gathering.

--- web/src/pages/services.tsx
+++ web/src/pages/services.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BackgroundImage from 'gatsby-background-image';
 import { Parallax } from 'react-scroll-parallax';
 import { graphql, Link, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import SEO from '../components/Seo';
 import styles from './services.module.scss';
 
@@ -17,10 +18,20 @@ const ServicesPage: React.FC = () => {
           }
         }
       }
+      gatheringImage: file(
+        relativePath: { eq: "manifestors-talk-gathering.jpg" }
+      ) {
+        childImageSharp {
+          fluid(quality: 80, srcSetBreakpoints: [800, 1200, 1600]) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
     }
   `);
 
   const backgroundServices = data.backgroundServices.childImageSharp.fluid;
+  const gatheringImage = data.gatheringImage.childImageSharp.fluid;
 
   return (
     <>
@@ -39,6 +50,61 @@ const ServicesPage: React.FC = () => {
           </Parallax>
           <Parallax y={['150px', '50px']}>
             <div className={`${styles.contentOverlay} ${styles.mAuto}`}>
+              <h2>Manifestor Online Gathering</h2>
+              <figure className={styles.gatheringImage}>
+                <Img fluid={gatheringImage} alt="" />
+              </figure>
+              <p>Welcome to the inaugural Manifestors Talk online gathering</p>
+              <p>
+                The space is held by Milla, 6/2 Splenic Manifestor,
+                experimenting since 2012, and Leena, 3/5 Ego Manifestor,
+                experimenting since 2020.
+              </p>
+              <p>
+                We created this to come together and share our experience with
+                Strategy and Authority. This is a space safe for you to listen
+                and share, or not!, about your experiment. Whether you’re just
+                starting or years deep in the experiment, you are most welcome.
+              </p>
+              <p>
+                FYI: This is NOT a teaching of the Human Design system. We are
+                keeping it simple and profound.{' '}
+              </p>
+              <p>
+                And yes, there will be frequent use of creative, colorful
+                language.
+              </p>
+              <p>Much love, Milla & Leena </p>
+              <p>
+                NOTE: The Zoomlink will be sent to the email address you use to
+                pay. If you want it sent to another email, let me know.
+              </p>
+              <p className={styles.mb2}>Price: €15</p>
+              <form
+                action="https://www.paypal.com/cgi-bin/webscr"
+                method="post"
+                target="_blank"
+                id={`paypal`}
+              >
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="hidden" name="encrypted" value="LSQUT3ZRM4WUC" />
+                <input
+                  type="image"
+                  src="https://www.paypalobjects.com/en_US/SE/i/btn/btn_buynowCC_LG.gif"
+                  name="submit"
+                  alt="PayPal - The safer, easier way to pay online!"
+                />
+                <img
+                  alt=""
+                  src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
+                  width="1"
+                  height="1"
+                />
+              </form>
+            </div>
+            <div
+              className={`${styles.contentOverlay} ${styles.mhAuto} ${styles.mt4}`}
+            >
               <h2>Readings and sessions</h2>
               <h3>Foundation reading with Christian</h3>
               <p>
