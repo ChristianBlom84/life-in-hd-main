6152f39c671fb60ba62a5e05922938a3a8319411
Christian Blom <christian@christianblom.se>
2025-08-23 11:42:31 +0200

    Updated from node-sass to sass and changed from @import to @use.

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       resolve: `gatsby-plugin-sass`,
       options: {
         // eslint-disable-next-line @typescript-eslint/no-require-imports
-        implementation: require('node-sass'),
+        implementation: require('sass'),
       },
     },
     `gatsby-plugin-sitemap`,
